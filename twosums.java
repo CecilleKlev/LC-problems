c738718f32f2d9b0cd5e0d1499c8619991d7fe0c
CecilleKlev <cecilleklev@gmail.com>
2023-09-23 13:09:26 +0200

    added type of topic

--- twosums.java
+++ twosums.java
@@ -4,6 +4,8 @@ return indices of the two numbers such that they add up to "target"
 You may assume that each input would have exactly one solution, and you may not use 
 the same element twice.
 
+TOPIC: Array
+
  */
 
 class Solution{
